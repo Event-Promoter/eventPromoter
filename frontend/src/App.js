086eebcaf101b8560d81f8f831d4acb357a028fb
AbhiAvati <Abhiavati20>
2021-03-22 21:15:01 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="app">
+      <Navbar/>
+      <Banner/>
+      
     </div>
   );
 }
---
+++ frontend/src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+    return (
+        <header className='banner'
+        style={{
+            backgroundSize: "cover",
+            backgroundImage: 'url("https://s3-alpha-sig.figma.com/img/eeba/1f02/8c5e4c09a4094a4989533c1adf1ffff9?Expires=1617580800&Signature=bBIjcKUoP6jOvxkGAk94Xyam2t5TTpGpntoZ~4vZ9s~jT~9n5gcVXoDjwKCWJP-u-ee0PuDhS8xGUG~NNQjq6zlo0a5MMauEH07Ag~kGwEcHoQrkCRMnHr2nNBC7ehgl7cV-sW6TXVbr6Xk7Oq5iN0ywU8podsRD01Sxx9ZL2Ne6jURyb1cJwz4dkQdYAVG30nyoETwi6Lo-QXsCxP7d8HqWGoQzeQYF99tTjKyrWiRj4wCI7Hw1SrzYU72UcQN5f~TeAGIkn0BMzty5DLy2qAn1EzD~Y-IIHDa3F~vPGyRrAiu11oMYSgWs9Muy1DADHSBESIqz~GYdDG8tb1q1Ww__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")',
+            backgroundPosition: " center",
+        }}
+        >
+
+        </header>
+    )
+}
+
+export default Banner
