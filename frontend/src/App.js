3460b73d9a930cf4615533c282441ee70c38b8b5
ADWAIT <adwaitnsk.2009@rediffmail.com>
2021-03-25 18:40:09 +0530

    grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Banner from './components/Banner';
 import Navbar from './components/Navbar';
+import Random from './components/random'
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
       <Banner/>
+      <Random/>
     </div>
   );
 }
