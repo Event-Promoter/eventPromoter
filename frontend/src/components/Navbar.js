0f87d12360e1638f120edd3927286a90356918ff
Ameya Ganore <ameyaganore@gmail.com>
2021-03-27 15:37:12 +0530

    Added Sigin/up button and minute changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,54 +1,66 @@
-import React, {useState,useEffect} from 'react'
-import logo from './logo.png';
-import './Navbar.css'
+import React, { useState, useEffect } from "react";
+import logo from "./logo1.svg";
+import "./Navbar.css";
 // import SearchIcon from '@material-ui/icons/Search';
 // import { IconButton } from '@material-ui/core';
-import Close from '@material-ui/icons/Close';
-import MenuIcon from '@material-ui/icons/Menu'
+import Close from "@material-ui/icons/Close";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Button } from "@material-ui/core";
+
 function Navbar() {
-    const [show,handleShow]=useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll",()=>{
-            if(window.scrollY>100){
-                handleShow(true);
-            }
-            else{
-                handleShow(false);
-            }
-        });
-        return () => {
-            window.removeEventListener("scroll",handleShow());
-        }
-    }, [])
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    return (
-        <div className={`navbar ${show &&"navbar__black"}`}>
-            <div className="navbar__logo">
-                
-                <img src={logo} alt='logo' width='100px' height='50px'/>
-            </div>
-            <div className='navbar__right'>
-                <p>Home</p>
-                <p>About Us</p>
-                <p>Contact Us</p>
-            </div>
-            <div className='menu'>
-                <div className="mobile-menu" onClick={handleClick}>
-                {click ? (
-                    <Close className="menu-icon" />
-                    ) : (
-                    <MenuIcon className="menu-icon" />
-        )        }
-                </div>
-                <div className={` ${!click && "menu__option"}`}>
-                    <p>Home</p>
-                    <p>About Us</p>
-                    <p>Contact Us</p>
-                </div>
-            </div>
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", handleShow());
+    };
+  }, []);
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <div className={`navbar ${show && "navbar__black"}`}>
+      <div className="navbar__logo">
+        <img src={logo} alt="logo" width="100px" height="50px" />
+      </div>
+      <div className="navbar__right">
+        <p>Home</p>
+        <p>About Us</p>
+        <Button
+          style={{ textTransform: "none" }}
+          variant="contained"
+          color="primary"
+        >
+          Sign In/Up
+        </Button>
+      </div>
+      <div className="menu">
+        <div className="mobile-menu" onClick={handleClick}>
+          {click ? (
+            <Close className="menu-icon" />
+          ) : (
+            <MenuIcon className="menu-icon" />
+          )}
         </div>
-    )
+        <div className={` ${!click && "menu__option"}`}>
+          <p>Home</p>
+          <p>About Us</p>
+          <Button
+            style={{ textTransform: "none", marginLeft: "5px" }}
+            variant="contained"
+            color="primary"
+          >
+            Sign In/Up
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/random.js
+++ frontend/src/components/random.js
@@ -1,96 +1,102 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import image from './image2.gif'
-import image1 from './image1.gif'
-import './random.css'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import IconButton from "@material-ui/core/IconButton";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import image from "./image2.gif";
+import image1 from "./image1.gif";
+import "./random.css";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-        top:'0%',
-        paddingBottom:'5%'
-    },
-    gridList: {
-        width: 900,
-        height: 700,
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-        transform: 'translateZ(0)',
-    },
-    titleBar: {
-        background:
-            'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-            'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
-    icon: {
-        color: 'white',
-    },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: "#eeeeee",
+    top: "0%",
+    paddingBottom: "5%",
+  },
+  gridList: {
+    width: 900,
+    height: 700,
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: "translateZ(0)",
+  },
+  titleBar: {
+    background:
+      "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
+      "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+  },
+  icon: {
+    color: "white",
+  },
 }));
 
 const tileData = [
-    {
-        img: image,
-        title: 'Non-Tech',
-        author: 'author',
-        featured: true,
-    },
-    {
-        img: image1,
-        title: 'Beta-Pitches',
-        author: 'author',
-    },
-    {
-        img: image1,
-        title: 'Hackathons',
-        author: 'author',
-    },
-    {
-        img: image,
-        title: 'Busines Events',
-        author: 'author',
-        featured: true,
-    }
-
+  {
+    img: image,
+    title: "Non-Tech",
+    author: "author",
+    featured: true,
+  },
+  {
+    img: image1,
+    title: "Beta-Pitches",
+    author: "author",
+  },
+  {
+    img: image1,
+    title: "Hackathons",
+    author: "author",
+  },
+  {
+    img: image,
+    title: "Business Events",
+    author: "author",
+    featured: true,
+  },
 ];
 
 export default function Random() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className='section2'>
-        <div className={classes.root}>
-            <GridList cellHeight={200} spacing={8} className={classes.gridList}>
-                {tileData.map((tile) => (
-                    <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 1.3 : 1}>
-                        <img className="image" src={tile.img} alt={tile.title} />
-                        <div class="overlay">
-                            <div class="text">{tile.title}</div>
-                        </div>
-                        {/* <Link to="/"> */}
-                            <GridListTileBar
-                                title={tile.title}
-                                titlePosition="top"
-                                actionIcon={
-                                    <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
-                                        <StarBorderIcon />
-                                    </IconButton>
-                                }
-                                actionPosition="left"
-                                className={classes.titleBar}
-                            />
-                        {/* </Link> */}
-                    </GridListTile>
-                ))}
-            </GridList>
-        </div>
-        </div>
-    );
+  return (
+    <div className="section2">
+      <div className={classes.root}>
+        <GridList cellHeight={200} spacing={8} className={classes.gridList}>
+          {tileData.map((tile) => (
+            <GridListTile
+              key={tile.img}
+              cols={tile.featured ? 2 : 1}
+              rows={tile.featured ? 1.3 : 1}
+            >
+              <img className="image" src={tile.img} alt={tile.title} />
+              <div class="overlay">
+                <div class="text">{tile.title}</div>
+              </div>
+              {/* <Link to="/"> */}
+              <GridListTileBar
+                title={tile.title}
+                titlePosition="top"
+                actionIcon={
+                  <IconButton
+                    aria-label={`star ${tile.title}`}
+                    className={classes.icon}
+                  >
+                    <StarBorderIcon />
+                  </IconButton>
+                }
+                actionPosition="left"
+                className={classes.titleBar}
+              />
+              {/* </Link> */}
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    </div>
+  );
 }
